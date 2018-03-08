91130c0a6c0fdcd7d16576cc2c9a7be4fe45bd77
cgebe <gebendorfer@icewave.org>
2018-03-08 16:11:37 +0100

    summarization adjustment, removal of duplicate header section in paragraphs

--- jrc/summarization.pl
+++ jrc/summarization.pl
@@ -20,7 +20,8 @@ my %options = (
     'outDir'    => 'out/',
 );
 
-my @languages = ( 'de', 'en', 'es', 'fr', 'it', 'sv' );
+my @languages = ( 'cs', 'de', 'en', 'es', 'fr', 'it', 'sv' );
+#my @languages = ('de');
 
 binmode( STDOUT, "utf8" );
 binmode( STDIN,  "utf8" );
@@ -34,7 +35,7 @@ my $select  = 0;
 my $sizesel = 0;
 
 # create tesdoc map
-open( my $testInfo, "<:encoding(utf8)", "test.info" )
+open( my $testInfo, "<:encoding(utf8)", "test-summary.info" )
   || die "Problems opening file test.info: $!\n";
 
 my %testDocs;
@@ -44,87 +45,110 @@ while ( my $line = <$testInfo> ) {
 }
 
 foreach my $lang (@languages) {
-    my $doc = 0;
-    open( my $summaries, ">:encoding(utf8)", "out/summarization/jrc_acquis.".$lang.".summaries" )
+    my $testdoc = 0;
+    my $doc     = 0;
+    my $skipped = 0;
+    open( my $summaries,
+        ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . ".summaries" )
       || die "Problems opening file summaries out: $!\n";
-    open( my $fulltexts, ">:encoding(utf8)", "out/summarization/jrc_acquis.".$lang.".fulltexts" )
+    open( my $fulltexts,
+        ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . ".fulltexts" )
       || die "Problems opening file fulltexts out: $!\n";
-    open( my $docinfo, ">:encoding(utf8)", "out/summarization/jrc_acquis.".$lang.".info" )
+    open( my $docinfo, ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . ".info" )
       || die "Problems opening file doc info out: $!\n";
+    open( my $testsummaries,
+        ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . "-test.summaries" )
+      || die "Problems opening file summaries out: $!\n";
+    open( my $testfulltexts,
+        ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . "-test.fulltexts" )
+      || die "Problems opening file fulltexts out: $!\n";
+    open( my $testdocinfo,
+        ">:encoding(utf8)",
+        "out/summarization/jrc_acquis." . $lang . "-test.info" )
+      || die "Problems opening file doc info out: $!\n";
+      open( my $skipinfo,
+          ">:encoding(utf8)",
+          "out/summarization/jrc_acquis." . $lang . "-skip.info" )
+        || die "Problems opening file doc info out: $!\n";
 
     my @yearFolders = grep { -d } glob 'acquis/' . $lang . '/*';
     foreach my $yearFolder (@yearFolders) {
         my @documents = glob( $yearFolder . '/*' );
         foreach my $document (@documents) {
             my @paragraphs = &loadParagraphs($document);
-            my $summary = shift @paragraphs;
+            my $summary    = shift @paragraphs;
+            my $start = 0;   # do not add the first paragraph ever, same as head
             my $fulltext = "";
+            my $acc     = "";
+            my $maxParagraphs = 0;
             foreach my $paragraph (@paragraphs) {
-                $fulltext .= $paragraph . " ";
+                if ($start) {
+                    $fulltext .= $paragraph . " ";
+                } else {
+                    $acc .= ' ' . $paragraph;
+                    my $similarity = compare($summary, $acc);
+                    if ($similarity >= 0.5) {
+                        $start = 1;
+                    }
+                    if ($maxParagraphs >= 6) {
+                        last;
+                    }
+                    $maxParagraphs++;
+                }
             }
-            print $summaries $summary."\n";
-            print $fulltexts $fulltext."\n";
-            print $docinfo $document."\n";
-            $doc++;
+            if ($start == 0) {
+                print $skipinfo $document . "\n";
+                $skipped++;
+            } else {
+                my $id = ( split( "-", ( ( split( "/", $document ) )[3] ) ) )[0];
+                if ( $testDocs{$id} ) {
+
+                    # test doc
+                    print $testsummaries $summary . "\n";
+                    print $testfulltexts $fulltext . "\n";
+                    print $testdocinfo $id . "\n";
+                    $testdoc++;
+                }
+                else {
+                    print $summaries $summary . "\n";
+                    print $fulltexts $fulltext . "\n";
+                    print $docinfo $id . "\n";
+                    $doc++;
+                }
+            }
         }
     }
-    print $lang." documents: " . $doc;
+    print $lang. " documents: " . $doc . " test documents: " . $testdoc . " skipped: " . $skipped. "\n";
     close($summaries);
     close($fulltexts);
     close($docinfo);
-}
-
-sub loadTestInfo {
-    my ( $lg1, $lg2 ) = @_;
-    my %testInfo;
-    my $testFile = "ac-test.info";
-
-    open( my $F, "<:encoding(utf8)", $testFile )
-      || do { warn "No test file found, continue"; return (); };
-    while ( my $line = <$F> ) {
-        my ( $docid, $remains ) =
-          split( /\s/, $line, 2 );  # 0: docid | 1: bg:5,cs:4,da:5,de:5,el:3,...
-        $remains =~ s/\s+$//;       #  remove trailing spaces
-        my @samples = split( /,/, $remains )
-          ;    # 0: bg:5 | 1: cs:4 | 2: da:5 | 3: de:5 | 4: el:3,...
-        if ( !$testInfo{$docid} ) {
-            $testInfo{$docid} = {};
-        }
-
-        # init hack
-        foreach my $sample (@samples) {
-            my @spec = split( /:/, $sample );    # 0: bg | 1: 5
-            if ( ( $lg1 eq $spec[0] || $lg2 eq $spec[0] )
-                && !$testInfo{$docid}->{ $spec[0] } )
-            {
-                $testInfo{$docid}->{ $spec[0] } = [];
-            }
-        }
-
-        foreach my $sample (@samples) {
-            my @spec = split( /:/, $sample );    # 0: bg | 1: 5
-            if ( $lg1 eq $spec[0] || $lg2 eq $spec[0] ) {
-                push $testInfo{$docid}->{ $spec[0] }, $spec[1];
-            }
-        }
-    }
-    close $F;
-    return %testInfo;
+    close($testsummaries);
+    close($testfulltexts);
+    close($testdocinfo);
+    close($skipinfo);
 }
 
 sub loadParagraphs {
-    my ( $file ) = @_;
+    my ($file) = @_;
     my @paragraphs = ();
 
     open( my $F, "<:encoding(utf8)", $file )
       || die "Problems opening file $file: $!\n";
     while ( my $line = <$F> ) {
         if ( $line =~ /<head n=\"([^\"]+)\">((.|\n|\r)*)<\/head>/i ) {
+
             #print  $1,"\t", $2,"\n";
-            push @paragraphs, $2; # push head onto paragraphs
+            push @paragraphs, $2;    # push head onto paragraphs
         }
+
         # print "LINE:",$line;
         if ( $line =~ /<p n=\"([^\"]+)\">((.|\n|\r)*)<\/p>/i ) {
+
             #print  $1,"\t", $2,"\n";
             push @paragraphs, $2;
         }
@@ -133,19 +157,34 @@ sub loadParagraphs {
     return @paragraphs;
 }
 
-sub getSentence {
-    my ( $lg, $targets1 ) = @_;
-    my $line = "";
-    my @targets1 = split( /[\s]+/, $targets1 );
+sub compare {
+    my ( $str1, $str2 ) = @_;
+    my $tok_str1 = tokenize($str1);
+    my $tok_str2 = tokenize($str2);
 
-    # discard insert or deletetion
-    if ( scalar @targets1 > 0 ) {
-        foreach my $t (@targets1) {
-            $line .= $text{$lg}->{s}->[$t] . " ";
-        }
-        $line =~ s/[\s]+$//;
+    # make a lookup hash for the smaller numer of tokens in str2
+    my %h;
+    @h{@$tok_str2} = ();    # slice syntax if fastest
+                            # now scan str1 for these tokens and count
+    my $found = 0;
+    for my $tok (@$tok_str1) {
+        $found++ if exists $h{$tok};
     }
-    return $line;
+    my $similarity = $found / @$tok_str1;
+    return $similarity;
+}
+
+sub tokenize {
+    my ($str) = @_;
+
+    # remove punctuation stuff
+    $str =~ s/[^A-Za-z0-9 ]+//g;
+
+    # lowercase
+    $str = lc $str;
+
+    # magic whitespace split and return array ref
+    return [ split ' ', $str ];
 }
 
 __END__
